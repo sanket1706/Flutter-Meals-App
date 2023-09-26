1dadcef9c196aa691d45031d74977dc2ce83cdd7
Sanket Mane <sanket.mane@pidilite.vc>
2023-09-26 16:34:51 +0530

    Added data modals and dummy data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,18 +12,16 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pick you category"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(16),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text("1"),
-          Text("2"),
-          Text("3"),
-          Text("4"),
-          Text("5"),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
